e30e662254120087b97e0a732f33e2a2f4d59f78
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2022-07-01 19:38:51 +0200

    Issue35312: LFL migration - More module changes

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -51,10 +51,10 @@ renamed_modules = {
     'sg_member_import_kfum': 'sg_member_kfum',
     'sg_member_subscription_central': 'sg_member_contract_central',
     # LFL Custom modules to SG Member
-    'partner_member_number': 'sg_member_organization',
+    # 'partner_member_number': 'sg_member_organization',
     'partner_ckr': 'sg_member_ckr',
     'partner_school': 'sg_member_school',
-    'sg_event': 'sg_member_event'
+    'sg_event': 'sg_member_event',
     
 }
 
