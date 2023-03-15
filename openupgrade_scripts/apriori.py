1f8064c341e27e7fde9a1d7821a8adb3b31be464
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2023-03-15 19:32:37 +0100

    backend_theme_v14 -> hue_backend_theme

--- openupgrade_scripts/apriori.py
+++ openupgrade_scripts/apriori.py
@@ -20,6 +20,7 @@ renamed_modules = {
     # OCA/...
     # S&G
     "mail_subject": "mail_notification_custom_subject",
+    "backend_theme_v14": "hue_backend_theme",
     # Google Maps OCA -> Brain-tec
     "base_google_map": "web_google_maps",
     "contacts_maps": "web_view_google_map",
