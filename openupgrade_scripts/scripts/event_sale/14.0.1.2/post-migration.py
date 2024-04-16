c9d99e1f9f9b928312ecaa4ea94aad36530ee0b7
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2024-04-16 10:54:07 +0200

    Issue48803: We need  event_sale.event_type_data_sale

--- openupgrade_scripts/scripts/event_sale/14.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/event_sale/14.0.1.2/post-migration.py
@@ -4,6 +4,7 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "event_sale", "14.0.1.2/noupdate_changes.xml")
-    openupgrade.delete_records_safely_by_xml_id(
-        env, [("event_sale.event_type_data_sale")]
-    )
+    # This event type is in use. Issue#48803
+    # openupgrade.delete_records_safely_by_xml_id(
+    #    env, [("event_sale.event_type_data_sale")]
+    # )
