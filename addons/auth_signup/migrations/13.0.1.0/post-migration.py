faf53bf0032e25103baee82d9448b25187bc6c03
JonathanStein <mail@jonathanstein.dk>
2024-04-26 11:08:41 +0200

    Do not change existing mail templates

--- addons/auth_signup/migrations/13.0.1.0/post-migration.py
+++ addons/auth_signup/migrations/13.0.1.0/post-migration.py
@@ -6,4 +6,6 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.load_data(env.cr, 'auth_signup', 'migrations/13.0.1.0/noupdate_changes.xml')
+    # Do not change existing templates
+    pass
+    # openupgrade.load_data(env.cr, 'auth_signup', 'migrations/13.0.1.0/noupdate_changes.xml')
