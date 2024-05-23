46d2efaa03607c85809e578ad7974f54ee511398
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2024-05-23 20:52:51 +0200

    Issue49168: Skip  openupgrade.disable_invalid_filters

--- openupgrade_scripts/scripts/base/15.0.1.3/end-migration.py
+++ openupgrade_scripts/scripts/base/15.0.1.3/end-migration.py
@@ -6,4 +6,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     """Call disable_invalid_filters in every edition of openupgrade"""
-    openupgrade.disable_invalid_filters(env)
+    # openupgrade.disable_invalid_filters(env)
+    pass
